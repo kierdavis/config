bbfd8f404b8d87ab4be91d04fb4ccf935e4ce09a
Kier Davis <kierdavis@gmail.com>
2017-06-24 18:54:57 +0100

    Add PySolFC package

--- nixos/extras/desktop/apps.nix
+++ nixos/extras/desktop/apps.nix
@@ -24,6 +24,7 @@ in {
     networkmanagerapplet
     pavucontrol
     pcb
+    pysolfc
     python27Packages.youtube-dl
     quasselClient
     screenshot
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,6 +9,7 @@ pkgs: pkgs // {
   lock = pkgs.callPackage ./lock { };
   mountext = pkgs.callPackage ./mountext { };
   passchars = pkgs.callPackage ./passchars { pythonPackages = pkgs.python27Packages; };
+  pysolfc = pkgs.callPackage ./pysolfc { };
   publish = pkgs.callPackage ./publish { };
   redstore = pkgs.callPackage ./redstore { };
   screenshot = pkgs.callPackage ./screenshot { eog = pkgs.gnome3.eog; };
