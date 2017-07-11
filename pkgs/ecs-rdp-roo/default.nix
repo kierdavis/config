b37055fcda18f72b51a35fb2b88b10f31c315c2c
Kier Davis <kierdavis@gmail.com>
2017-07-11 18:18:47 +0100

    Reduce dimensions of the remote desktop window created by ecs-rdp-roo, so that it doesn't fill the entire screen

--- pkgs/ecs-rdp-roo/default.nix
+++ pkgs/ecs-rdp-roo/default.nix
@@ -2,5 +2,5 @@
 
 writeScriptBin "ecs-rdp-roo" ''
   #!${stdenv.shell}
-  ${freerdp}/bin/xfreerdp /v:roo.ecs.soton.ac.uk /u:kad2g15 /w:1920 /h:1080
+  ${freerdp}/bin/xfreerdp /v:roo.ecs.soton.ac.uk /u:kad2g15 /w:800 /h:600
 ''
