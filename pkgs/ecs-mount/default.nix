e34db33c7f2a6ac4aa2a3b4cc144b78bc2ba0632
Kier Davis <kierdavis@gmail.com>
2017-10-03 18:14:45 +0100

    pkgs/ecs-mount: update to new filestore (not tested yet)

--- pkgs/ecs-mount/default.nix
+++ pkgs/ecs-mount/default.nix
@@ -4,5 +4,5 @@ writeScriptBin "ecs-mount" ''
   #!${stdenv.shell}
   uid=$(id --user kier)
   gid=$(id --group kier)
-  sudo ${cifs-utils}/bin/mount.cifs -o username=kad2g15,uid=$uid,forceuid,gid=$gid,forcegid,actimeo=60 //ugsamba.ecs.soton.ac.uk/kad2g15 /mnt/ecs
+  sudo ${cifs-utils}/bin/mount.cifs -o username=kad2g15,uid=$uid,forceuid,gid=$gid,forcegid,actimeo=60 //filestore.soton.ac.uk/users/kad2g15 /mnt/ecs
 ''
