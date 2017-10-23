0f6af670194ccbccfabbedf6799fbee14bc73706
Kier Davis <kierdavis@gmail.com>
2017-10-23 23:36:25 +0100

    soton-rdp: make RDP window larger

--- pkgs/soton-rdp/default.nix
+++ pkgs/soton-rdp/default.nix
@@ -2,5 +2,5 @@
 
 writeScriptBin "soton-rdp" ''
   #!${stdenv.shell}
-  ${freerdp}/bin/xfreerdp /v:rdbroker.soton.ac.uk /gd:remotedesktopgateway.soton.ac.uk /u:kad2g15 /gu:kad2g15 /w:800 /h:600
+  ${freerdp}/bin/xfreerdp /v:rdbroker.soton.ac.uk /gd:remotedesktopgateway.soton.ac.uk /u:kad2g15 /gu:kad2g15 /w:1840 /h:1000
 ''
