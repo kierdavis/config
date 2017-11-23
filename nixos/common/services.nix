1fcf13dacc7b8cfee527e351dfe30b73efec0d1e
Kier Davis <kierdavis@gmail.com>
2017-11-23 21:58:03 +0000

    Install the atd service on all machines

--- nixos/common/services.nix
+++ nixos/common/services.nix
@@ -5,6 +5,9 @@
   services.ntp.enable = false;
   services.timesyncd.enable = true;
 
+  # enable atd (a one-shot command scheduler)
+  services.atd.enable = true;
+
   # ssh server
   services.openssh = {
     enable = true;
