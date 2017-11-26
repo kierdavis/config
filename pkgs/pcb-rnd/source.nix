8ef81f531efe04c146a1184767255ff9870e3c75
Kier Davis <kierdavis@gmail.com>
2017-11-26 02:37:10 +0000

    pcb-rnd: freeze all SVN externals
    Otherwise, the sha256 is invalidated whenever someone commits
    to any external, since they are not locked to particular revisions.
    
    This also updates pcb-rnd from r12693 to r13035.

---
+++ pkgs/pcb-rnd/source.nix
@@ -0,0 +1,34 @@
+{ stdenv, fetchsvn }:
+
+with stdenv.lib;
+
+let
+  svnSources = import ./svn-sources.nix { inherit fetchsvn; };
+
+  # SVN revision of the top-level repo.
+  rev = svnSources.".".rev;
+
+  # Returns a shell script that will overlay the contents of ${src}
+  # onto $out/${reldest}.
+  buildStep = reldest: src: ''
+    dest=$out/${reldest}
+    echo 'Placing ${src.url}@${src.rev} at '$dest
+    mkdir -p $dest
+    cp --recursive ${src}/* --target-directory=$dest
+    # cp will copy the permissions from the source files in the Nix store, which are
+    # all read-only. We may need to overlay later svn sources on top of this one, so
+    # we should mark the directory as writeable to enable this.
+    chmod -R +w $dest
+  '';
+
+  buildScript = concatStrings (mapAttrsToList buildStep svnSources);
+
+in stdenv.mkDerivation rec {
+  inherit rev;
+
+  name = "pcb-rnd-source-${version}";
+  version = "svn-r${rev}";
+
+  phases = [ "buildPhase" ];
+  buildPhase = buildScript;
+}
