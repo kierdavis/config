637fb8af49fe0d9276e1d6beb5e30d8706f21ef6
Kier Davis <kierdavis@gmail.com>
2018-01-01 01:48:18 +0000

    Add useful flags to run-locally.sh

--- campanella/run-locally.sh
+++ campanella/run-locally.sh
@@ -7,4 +7,4 @@ dir=$PWD/containers/$name
 
 image=$(nix-build --no-out-link $dir/dockerimage.nix)
 docker load < $image
-docker run $(cat $dir/dockerflags.txt) campanella-$name
+docker run --rm --interactive --tty $(cat $dir/dockerflags.txt) campanella-$name
