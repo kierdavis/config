87b9831c2c2da4bd72b19fa09d033e0a76ab25bf
Kier Davis <kierdavis@gmail.com>
2018-01-06 14:46:21 +0000

    pkgs/screenshot: fix typo

--- pkgs/screenshot/default.nix
+++ pkgs/screenshot/default.nix
@@ -6,7 +6,7 @@ writeScriptBin "screenshot" ''
   dir=/tmp/screenshots
   mkdir -p "$dir"
   now=`date +'%Y-%m-%d_%H:%M:%S'`
-  filename="dir/$now.png"
+  filename="$dir/$now.png"
   ${imagemagick}/bin/import "$filename"
   ${eog}/bin/eog "$filename" &
 ''
