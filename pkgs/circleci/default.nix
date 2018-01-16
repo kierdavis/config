0332a315a132117323eda8f4ba630db96e05ca5c
Kier Davis <kierdavis@gmail.com>
2018-01-16 18:24:16 +0000

    circleci: freeze the wrapper script in this repo, since the upstream source changes too often (and is unversioned)

--- pkgs/circleci/default.nix
+++ pkgs/circleci/default.nix
@@ -3,11 +3,7 @@
 stdenv.mkDerivation {
   name = "circleci";
 
-  src = fetchurl {
-    url = "https://circle-downloads.s3.amazonaws.com/releases/build_agent_wrapper/circleci";
-    sha256 = "12q0ymnzzz4pi79zy3wblbrxkd0dg2s84z6ysm4mvqs6hhs7m6qh";
-    executable = true;
-  };
+  src = ./circleci;
 
   buildInputs = [ makeWrapper ];
 
