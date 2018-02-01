3eb1888791ccf6c056ee460f999944f5eba259a8
Kier Davis <kierdavis@gmail.com>
2018-02-01 19:53:27 +0000

    Clean up remnants of campanella-vpnserver

--- campanella/list-clients.sh
+++ campanella/list-clients.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Doesn't work at the moment - either need to fix the openvpn bug or downgrade openvpn
+# on the server.
+
 set -o errexit -o pipefail -o nounset
 
 echo "status 3" | nc -N campanella-vpnserver 9000 | grep '^ROUTING_TABLE' | cut -f 3,2
