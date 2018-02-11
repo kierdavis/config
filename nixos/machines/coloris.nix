bb32baa171991eb293c7fea413d19d134429b79e
Kier Davis <kierdavis@gmail.com>
2018-02-11 14:43:34 +0000

    coloris: put docker data on its own filesystem so it doesn't clutter up the root FS

--- nixos/machines/coloris.nix
+++ nixos/machines/coloris.nix
@@ -70,6 +70,11 @@ in
     device = "/dev/disk/by-uuid/c522b9a8-b534-428c-9e4e-6d297ed9dba4";
     fsType = "ext4";
   };
+  fileSystems."/var/lib/docker" = {
+    device = "/dev/disk/by-uuid/842e7d6c-cc65-4719-89b4-3968b8bfb30d";
+    fsType = "ext4";
+  };
+  systemd.services.docker.after = [ "var-lib-docker.mount" ];
 
   environment.systemPackages = [
     localPkgs.boincgpuctl
