1ffca1a47e2ee570a28ed72e432f30a95236d099
Kier Davis <kierdavis@gmail.com>
2018-02-17 16:56:02 +0000

    Actually use the correct jemalloc patch

--- patches/jemalloc/default.nix
+++ patches/jemalloc/default.nix
@@ -1,31 +1,27 @@
 # https://github.com/NixOS/nixpkgs/pull/33260 is not yet available on the current release channel.
 # Therefore we take the fixed version of the file and overwrite our current package set with it.
-# From https://github.com/NixOS/nixpkgs/blob/03d78aea0d7ffc2091edd2bbc98124a7b2fd9934/pkgs/development/libraries/jemalloc/default.nix
+# From https://github.com/NixOS/nixpkgs/blob/e9d5c55d01102f322b2b2a7d10f2d7335b1c968d/pkgs/development/libraries/jemalloc/default.nix
 
-{ stdenv, fetchurl,
-  # jemalloc is unable to correctly detect transparent hugepage support on
-  # ARM, and the default kernel ARMv6/7 kernel does not enable it, so we 
-  # explicitly disable support
-  thpSupport ? stdenv.system != "armv7l-linux" &&
-               stdenv.system != "armv6l-linux" }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
   name = "jemalloc-${version}";
-  version = "4.5.0";
+  version = "5.0.1";
 
   src = fetchurl {
     url = "https://github.com/jemalloc/jemalloc/releases/download/${version}/${name}.tar.bz2";
-    sha256 = "9409d85664b4f135b77518b0b118c549009dc10f6cba14557d170476611f6780";
+    sha256 = "4814781d395b0ef093b21a08e8e6e0bd3dab8762f9935bbfb71679b0dea7c3e9";
   };
 
   # By default, jemalloc puts a je_ prefix onto all its symbols on OSX, which
   # then stops downstream builds (mariadb in particular) from detecting it. This
   # option should remove the prefix and give us a working jemalloc.
   configureFlags = stdenv.lib.optional stdenv.isDarwin "--with-jemalloc-prefix="
-                   ++ stdenv.lib.optional (!thpSupport) "--disable-thp";
-
+                   # jemalloc is unable to correctly detect transparent hugepage support on
+                   # ARM (https://github.com/jemalloc/jemalloc/issues/526), and the default
+                   # kernel ARMv6/7 kernel does not enable it, so we explicitly disable support
+                   ++ stdenv.lib.optional stdenv.isArm "--disable-thp";
   doCheck = true;
-
 
   meta = with stdenv.lib; {
     homepage = http://jemalloc.net;
