5958156bdc4d1b40f9b8ce19b20a0eca32aaeaf8
Kier Davis <kierdavis@gmail.com>
2018-02-18 14:23:11 +0000

    Use a README for patch packages

--- patches/jemalloc/default.nix
+++ patches/jemalloc/default.nix
@@ -1,7 +1,3 @@
-# https://github.com/NixOS/nixpkgs/pull/33260 is not yet available on the current release channel.
-# Therefore we take the fixed version of the file and overwrite our current package set with it.
-# From https://github.com/NixOS/nixpkgs/blob/e9d5c55d01102f322b2b2a7d10f2d7335b1c968d/pkgs/development/libraries/jemalloc/default.nix
-
 { stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
