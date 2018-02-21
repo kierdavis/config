a7a39e4b8ccb4a192cad9d2584217ca0e9db304d
Kier Davis <kierdavis@gmail.com>
2018-02-21 10:22:56 +0000

    Refactor nixos/default.nix

--- nixos/default.nix
+++ nixos/default.nix
@@ -3,39 +3,33 @@ let
   nixos = import <nixpkgs/nixos>;
 in
   rec {
-    machines = {
-      coloris = (nixos {
-        configuration = machines/coloris.nix;
-        system = "x86_64-linux";
-      }).system;
+    coloris = (nixos {
+      configuration = machines/coloris.nix;
+      system = "x86_64-linux";
+    }).system;
 
-      ouroboros = (nixos {
-        configuration = machines/ouroboros.nix;
-        system = "x86_64-linux";
-      }).system;
+    ouroboros = (nixos {
+      configuration = machines/ouroboros.nix;
+      system = "x86_64-linux";
+    }).system;
 
-      nocturn = (nixos {
-        configuration = machines/nocturn.nix;
-        system = "x86_64-linux";
-      }).system;
+    nocturn = (nixos {
+      configuration = machines/nocturn.nix;
+      system = "x86_64-linux";
+    }).system;
 
-      saelli = (nixos {
-        configuration = machines/saelli.nix;
-        system = "x86_64-linux";
-      }).system;
+    saelli = (nixos {
+      configuration = machines/saelli.nix;
+      system = "x86_64-linux";
+    }).system;
 
-      campanella2 = (nixos {
-        configuration = machines/campanella2.nix;
-        system = "x86_64-linux";
-      }).system;
+    campanella2 = (nixos {
+      configuration = machines/campanella2.nix;
+      system = "x86_64-linux";
+    }).system;
 
-      solanin = (nixos {
-        configuration = machines/solanin.nix;
-        system = "armv7l-linux";
-      }).system;
-    };
-
-    all = {
-      inherit (machines) coloris ouroboros nocturn saelli campanella2;
-    };
+    solanin = (nixos {
+      configuration = machines/solanin.nix;
+      system = "armv7l-linux";
+    }).system;
   }
