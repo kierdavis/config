a9b807a047b4e50ff79754c0be80db4959cae7dd
Kier Davis <kierdavis@gmail.com>
2018-10-30 15:57:32 +0000

    Fix proxmox container platform

--- nixos/extras/platform/proxmox-ct.nix
+++ nixos/extras/platform/proxmox-ct.nix
@@ -1,3 +1,5 @@
+{ config, lib, pkgs, ... }:
+
 {
   imports = [
     <nixpkgs/nixos/modules/virtualisation/lxc-container.nix>
@@ -5,6 +7,18 @@
 
   # Fix multiple agetty instances being spawned on the same terminal.
   systemd.services."getty@".unitConfig = {
-    ConditionVirtualisation = "!lxc";
+    ConditionVirtualization = "!lxc";
   };
+
+  # Fix "unable to make '/' private mount: Permission denied"
+  nix.useSandbox = lib.mkForce false;
+
+  # Fix /dev/net/tun being unavailable.
+  # https://vroomtech.io/enable-tuntap-in-a-proxmox-lxc-container/
+  boot.postBootCommands = ''
+    if [ ! -c /dev/net/tun ]; then
+      mkdir -p /dev/net
+      mknod -m 666 /dev/net/tun c 10 200
+    fi
+  '';
 }
