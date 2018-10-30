50a412f55804b1a3856e13e63f7924fd63e40571
Kier Davis <kierdavis@gmail.com>
2018-10-30 18:37:17 +0000

    Run CUPS server only on bonito

--- nixos/common/print.nix
+++ nixos/common/print.nix
@@ -1,13 +1,4 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.printing = {
-    enable = true;
-    drivers = [
-      pkgs.gutenprint
-      pkgs.samsung-unified-linux-driver_4_01_17
-    ];
-  };
-
-  environment.variables.PRINTER = "woodside";
 }
