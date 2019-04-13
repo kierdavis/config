82ca233bd9a9d55db9f1813619bd7fbd07911644
Kier Davis <kierdavis@gmail.com>
2019-04-13 12:08:02 +0100

    Add expression for libkoki

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -10,6 +10,7 @@ self: super: {
   hartmaster = super.callPackage ./hartmaster { };
   home-manager = super.callPackage ./home-manager { };
   i3blocks-scripts = super.callPackage ./i3blocks-scripts { inherit (self.linuxPackages) nvidia_x11; };
+  libkoki = super.callPackage ./libkoki { };
   lock = super.callPackage ./lock { };
   magic = super.callPackage ./magic { };
   mountext = super.callPackage ./mountext { };
