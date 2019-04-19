bd6e02d74cab34938613f09477d9d70e1bd104af
Kier Davis <kierdavis@gmail.com>
2019-04-19 14:45:39 +0100

    Fix passmenu/pinentry focus issue (see dmenu-focus.patch for details)

--- patches/overlay.nix
+++ patches/overlay.nix
@@ -1,3 +1,6 @@
 self: super: {
+  dmenu = super.dmenu.overrideDerivation (oldAttrs: {
+    patches = (if oldAttrs.patches != null then oldAttrs.patches else []) ++ [ ./dmenu-focus.patch ];
+  });
   openvpn23 = super.callPackage ./openvpn23 { };
 }
