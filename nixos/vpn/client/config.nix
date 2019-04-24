984711c6b450c299fe2fa2b4ba46a23ec7576bb5
Kier Davis <kierdavis@gmail.com>
2019-04-24 23:40:01 +0100

    fix legacy VPN

--- nixos/vpn/client/config.nix
+++ nixos/vpn/client/config.nix
@@ -4,7 +4,7 @@ let
   template = ./config.template;
   config = runCommand "client.conf" {
     inherit clientCert clientKey;
-    remoteHost = "campanella2";
+    remoteHost = (import ../../cascade.nix).addrs."public.campanella2.h.cascade";
     remotePort = 1194;
     caCert     = ../../../secret/vpn/certs/ca.crt;
     vpnHmacKey = ../../../secret/vpn/ta.key;
