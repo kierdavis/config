4aa21a503c8a4d2d98d5a47607e88ebe814a3e5c
Kier Davis <kierdavis@gmail.com>
2019-05-16 16:35:32 +0100

    Remove packages from config/pkgs that have since been mainlined into nixpkgs

--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -2,11 +2,9 @@ self: super: {
   admesh = super.callPackage ./admesh { };
   boincgpuctl = super.callPackage ./boincgpuctl { };
   circleci = super.callPackage ./circleci { };
-  freesweep = super.callPackage_i686 ./freesweep { };
   ftb-launcher = super.callPackage ./ftb-launcher { };
   gds2svg = super.callPackage ./gds2svg { };
   hartmaster = super.callPackage ./hartmaster { };
-  home-manager = super.callPackage ./home-manager { };
   i3blocks-scripts = super.callPackage ./i3blocks-scripts { inherit (self.linuxPackages) nvidia_x11; };
   klayout = super.libsForQt5.callPackage ./klayout { };
   libkoki = super.callPackage ./libkoki { };
@@ -17,7 +15,6 @@ self: super: {
   mstream = super.callPackage ./mstream { nodejs = self."nodejs-8_x"; };
   passchars = super.callPackage ./passchars { pythonPackages = self.python27Packages; };
   pcb-rnd = super.callPackage ./pcb-rnd { };
-  pysolfc = super.callPackage ./pysolfc { };
   publish = super.callPackage ./publish { };
   quartus = super.callPackage ./quartus { };
   redstore = super.callPackage ./redstore { };
