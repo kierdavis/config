1f23393a36bdc1ad96c75d4404a9cd59b40bc2ca
Kier Davis <kierdavis@gmail.com>
2019-05-16 17:05:54 +0100

    Define both IPv4 and IPv6 versions of publicly reachable servers
    and select which one to use when making connections based on whether
    the client has IPv6 Internet access.

--- nixos/vpn/client/config.nix
+++ nixos/vpn/client/config.nix
@@ -1,10 +1,12 @@
-{ clientCert, clientKey, runCommand }:
+{ clientCert, clientKey, runCommand, ipv6-internet }:
 
 let
   template = ./config.template;
+  campanella2-iface = if ipv6-internet then "public6" else "public4";
+  campanella2-addr = (import ../../cascade.nix).addrs."${campanella2-iface}.campanella2.h.cascade";
   config = runCommand "client.conf" {
     inherit clientCert clientKey;
-    remoteHost = (import ../../cascade.nix).addrs."public.campanella2.h.cascade";
+    remoteHost = campanella2-addr;
     remotePort = 1194;
     caCert     = ../../../secret/vpn/certs/ca.crt;
     vpnHmacKey = ../../../secret/vpn/ta.key;
