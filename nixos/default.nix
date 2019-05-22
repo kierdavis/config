d301044c55f164f1cadb4a5592cc6362b770d168
Kier Davis <kierdavis@gmail.com>
2019-05-22 01:45:47 +0100

    Clean up dead hosts

--- nixos/default.nix
+++ nixos/default.nix
@@ -8,11 +8,6 @@ in
       system = "x86_64-linux";
     }).system;
 
-    ouroboros = (nixos {
-      configuration = machines/ouroboros.nix;
-      system = "x86_64-linux";
-    }).system;
-
     saelli = (nixos {
       configuration = machines/saelli.nix;
       system = "x86_64-linux";
@@ -21,11 +16,6 @@ in
     campanella2 = (nixos {
       configuration = machines/campanella2.nix;
       system = "x86_64-linux";
-    }).system;
-
-    gyroscope = (nixos {
-      configuration = machines/gyroscope.nix;
-      system = "aarch64-linux";
     }).system;
 
     bonito = (nixos {
