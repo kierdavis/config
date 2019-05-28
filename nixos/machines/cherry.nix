d7f16c2b0eaa7a0d24aa9fac478df9cc01c61452
Kier Davis <kierdavis@gmail.com>
2019-05-28 18:09:28 +0100

    Reduce load on VMs

--- nixos/machines/cherry.nix
+++ nixos/machines/cherry.nix
@@ -77,7 +77,7 @@ in { config, lib, pkgs, ... }: {
     wifi = false;
     ipv6-internet = false;
     cpu = {
-      cores = 24;
+      cores = 16;
       intel = true;
     };
     netfs.cherry.isServer = true;
