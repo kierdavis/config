7b417268b3d28215480c9d2ae7fb08d96ecc0611
Kier Davis <kierdavis@gmail.com>
2019-06-18 12:50:27 +0100

    Fix duplicity not being able to find backblaze B2 library

--- patches/overlay.nix
+++ patches/overlay.nix
@@ -3,4 +3,7 @@ self: super: {
   dmenu = super.dmenu.overrideDerivation (oldAttrs: {
     patches = (if oldAttrs ? patches && oldAttrs.patches != null then oldAttrs.patches else []) ++ [ ./dmenu-focus.patch ];
   });
+  duplicity = super.duplicity.overrideDerivation (oldAttrs: {
+    propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ self.backblaze-b2 ];
+  });
 }
