e4b441dda93a472429de8fcefeec9441b99df375
Kier Davis <kierdavis@gmail.com>
2019-10-16 19:26:34 +0100

    Stop using that weird chromium patch

--- patches/overlay.nix
+++ patches/overlay.nix
@@ -1,5 +1,4 @@
 self: super: {
-  chromium = self.callPackage ./chromium {};
   duplicity = super.duplicity.overrideDerivation (oldAttrs: {
     propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ self.backblaze-b2 ];
   });
