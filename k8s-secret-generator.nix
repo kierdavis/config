5317cc9e7d98e623fc76929d4bad9880644e220a
Kier Davis <kierdavis@gmail.com>
2019-10-24 20:13:53 +0100

    Refactor k8s-secret-generator

--- k8s-secret-generator.nix
+++ k8s-secret-generator.nix
@@ -4,10 +4,10 @@ let
   network = import ./network.nix;
   vpnKeys = import ./secret/vpn-keys.nix;
 
-  mkYamlVal = value: if value ? filePath then
-    "$(base64 --wrap=0 <${value.filePath})"
+  mkYamlVal = value: if value ? fromFile then
+    "$(base64 --wrap=0 <${value.fromFile})"
   else
-    abort "secret value must define filePath";
+    abort "secret value must define fromFile";
 
   mkYaml = { name, entries }: namespace: runCommand "${namespace}.${name}.yaml" {} ''
     cat >$out <<EOF
@@ -26,39 +26,39 @@ let
     ingressAuthYaml = mkYaml {
       name = "ingress-auth";
       entries = {
-        "auth".filePath = ./secret/k8s-ingress.auth;
+        "auth".fromFile = ./secret/k8s-ingress.auth;
       };
     };
 
     vpnServerInterfaceConfig = writeText "vpn-server-wg0.conf" ''
       [Interface]
-      Address = ${network.dns."vpn.vpn-server.k8s.cascade".address}/24
+      Address = ${network.byName."vpn.vpn-server.k8s.cascade".address}/24
       PrivateKey = ${vpnKeys.server.priv}
       ListenPort = 5555
       [Peer]
       PublicKey = ${vpnKeys.saelli.pub}
-      AllowedIPs = ${network.dns."vpn.saelli.cascade".address}/32
+      AllowedIPs = ${network.byName."vpn.saelli.cascade".address}/32
     '';
 
     vpnServerConfigYaml = mkYaml {
       name = "vpn-server-config";
       entries = {
-        "wg0.conf".filePath = vpnServerInterfaceConfig;
+        "wg0.conf".fromFile = vpnServerInterfaceConfig;
       };
     };
 
     namespaces = [ "kier" "kier-dev" ];
     namespacedYamls = [ ingressAuthYaml vpnServerConfigYaml ];
     yamls = lib.concatMap (yamlFunc: map (namespace: yamlFunc namespace) namespaces) namespacedYamls;
-    yamlDir = runCommand "k8s-secret-yamls" {} ''
-      mkdir -p $out
-      ${lib.concatMapStringsSep "\n" (yaml: "ln -s ${yaml} $out/${yaml.name}") yamls}
+    combinedYaml = runCommand "k8s-secret-yamls" {} ''
+      for file in ${lib.concatStringsSep " " yamls}; do
+        cat $file >> $out
+        echo '---' >> $out
+      done
     '';
 
     syncScript = writeShellScript "sync-k8s-secrets" ''
-      for file in $(find ${yamlDir} -name '*.yaml'); do
-        kubectl apply -f $file
-      done
+      exec ${kubectl}/bin/kubectl apply -f ${combinedYaml}
     '';
   };
 
