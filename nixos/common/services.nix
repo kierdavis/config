5e4148ac25809acc80b8ee4d03ffd7dde1fdfd8c
Kier Davis <kierdavis@gmail.com>
2019-12-27 22:55:09 +0000

    Make services.timesyncd.enable overridable

--- nixos/common/services.nix
+++ nixos/common/services.nix
@@ -2,8 +2,11 @@
 
 {
   # use timesyncd instead of ntpd
+  # Explicit lower-than-usual priority of 150 is needed
+  # so that <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
+  # can override this setting at the default priority of 100.
   services.ntp.enable = false;
-  services.timesyncd.enable = true;
+  services.timesyncd.enable = lib.mkOverride 150 true;
 
   # enable atd (a one-shot command scheduler)
   services.atd.enable = true;
