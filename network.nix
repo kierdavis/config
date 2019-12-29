21ea9adf960f92082b32347d5480f79862fadea2
Kier Davis <kierdavis@gmail.com>
2019-12-29 18:03:19 +0000

    Declare new VPN

--- network.nix
+++ network.nix
@@ -15,13 +15,12 @@ in rec {
     # Public address space
     (mkA "pub4.beagle2.cascade" "176.9.121.81")
 
-    # VPN address space
-    (mkNet "vpn.network.cascade" "10.88.1.0" 24)
-    (mkA "vpn.vpn-server.k8s.cascade" "10.88.1.1")
-    (mkA "vpn.saelli.cascade" "10.88.1.2")
+    # Kubenetes VPN address space
+    (mkNet "k8s-vpn.network.cascade" "10.107.252.0" 24)
+    (mkA "k8s-vpn.beagle2.cascade" "10.107.252.1")
+    (mkA "k8s-vpn.butterfly.cascade" "10.107.252.2")
 
     # Aliases to default interfaces.
-    (mkCNAME "saelli.cascade" "vpn.saelli.cascade")
   ];
 
   byName = builtins.listToAttrs (map (record: { name = record.name; value = record; }) records);
