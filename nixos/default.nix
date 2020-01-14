a75e5991e5b9505633582516b1228d6b4c115d43
Kier Davis <kierdavis@gmail.com>
2020-01-14 19:47:53 +0000

    Refactor

--- nixos/default.nix
+++ nixos/default.nix
@@ -3,15 +3,15 @@ let
   nixos = import <nixpkgs/nixos>;
 in
   rec {
-    coloris = (nixos {
+    coloris = nixos {
       configuration = machines/coloris.nix;
       system = "x86_64-linux";
-    }).system;
+    };
 
-    saelli = (nixos {
+    saelli = nixos {
       configuration = machines/saelli.nix;
       system = "x86_64-linux";
-    }).system;
+    };
 
     butterfly = nixos {
       configuration = machines/butterfly.nix;
