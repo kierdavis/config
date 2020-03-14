192f3f56b2b6861d281254b19999dab19f468ec1
Kier Davis <kierdavis@gmail.com>
2020-03-14 19:37:22 +0000

    Enable all magic sysrq keys

--- nixos/common/boot.nix
+++ nixos/common/boot.nix
@@ -7,4 +7,7 @@
 
   # Microcode updates
   hardware.cpu.intel.updateMicrocode = config.machine.cpu.intel;
+
+  # Enable all magic sysrq keys (by default, only sync is enabled).
+  boot.kernel.sysctl."kernel.sysrq" = 1;
 }
