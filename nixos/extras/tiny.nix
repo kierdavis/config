695e7a2da39e4472227bc20409ba2f4523b53ef9
Kier Davis <kierdavis@gmail.com>
2020-03-16 13:31:37 +0000

    Add hax for speeding up a full rebuild

---
+++ nixos/extras/tiny.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, lib, ... }: {
+  nixpkgs.overlays = [ (import ../../patches/tiny.nix) ];
+  boot.plymouth.enable = lib.mkForce false;
+  fonts.fontconfig.enable = lib.mkForce false;
+  services.udisks2.enable = lib.mkForce false;
+  services.timesyncd.enable = lib.mkForce false;
+  services.atd.enable = lib.mkForce false;
+  services.syncthing.enable = lib.mkForce false;
+  security.pam.services.su.forwardXAuth = lib.mkForce false;
+  xdg.mime.enable = lib.mkForce false;
+  nix.gc.automatic = lib.mkForce false;
+  documentation = {
+    enable = lib.mkForce false;
+    nixos.enable = lib.mkForce false;
+    dev.enable = lib.mkForce false;
+    man.enable = lib.mkForce false;
+    info.enable = lib.mkForce false;
+    doc.enable = lib.mkForce false;
+  };
+}
