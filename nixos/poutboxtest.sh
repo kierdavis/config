747d24b95a1ee28be1819e897e42d399c6ab9851
Kier Davis <kierdavis@gmail.com>
2020-03-16 13:30:55 +0000

    Enable serial port for poutboxtest.sh

--- nixos/poutboxtest.sh
+++ nixos/poutboxtest.sh
@@ -2,5 +2,5 @@
 set -o errexit -o nounset -o pipefail
 result=$(nix-build -A poutbox.vm --no-out-link)
 rm -fv poutbox.qcow2
-export QEMU_OPTS="-device usb-ehci,id=ehci -device usb-host,hostbus=2,hostaddr=4,bus=ehci.0"
+export QEMU_OPTS="-serial stdio -device usb-ehci,id=ehci -device usb-host,hostbus=2,hostaddr=4,bus=ehci.0"
 $result/bin/run-poutbox-vm
