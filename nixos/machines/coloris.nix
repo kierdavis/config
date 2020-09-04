e915d733ca2522819fbeb6c561425c7765bd51f9
Kier Davis <me@kierdavis.com>
2020-09-05 00:44:44 +0100

    Configure printer driver

--- nixos/machines/coloris.nix
+++ nixos/machines/coloris.nix
@@ -72,4 +72,12 @@
   hardware.ckb-next.enable = true;
   # https://github.com/mattanger/ckb-next#linux
   boot.kernelParams = [ "usbhid.quirks=0x1B1C:0x1B15:0x20000408,0x1B1C:0x1B2F:0x20000408" ];
+
+  services.printing = {
+    enable = true;
+    drivers = [
+      pkgs.gutenprint
+      pkgs.cups-brother-hl1110
+    ];
+  };
 }
