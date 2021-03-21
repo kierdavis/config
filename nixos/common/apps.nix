14997850971d1a9b349845949132f38f581b7093
Kier Davis <me@kierdavis.com>
2021-03-21 14:58:24 +0000

    Add tmux config

--- nixos/common/apps.nix
+++ nixos/common/apps.nix
@@ -27,10 +27,27 @@ in {
     doc.enable = false; # anything that doesn't fall into the above two categories
   };
 
+  programs.tmux = {
+    enable = true;
+    escapeTime = 0;
+    extraConfig = ''
+      # There was some issue with Home and End keys not working properly, and this fixed it:
+      bind-key -n Home send Escape "OH"
+      bind-key -n End send Escape "OF"
+
+      # When creating new panes or windows, use the same working directory as the currently selected pane instead of defaulting to $HOME.
+      bind '"' split-window -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+      bind c new-window  # -c "#{pane_current_path}"
+
+      # This key did something really annoying (move current pane into its own window, perhaps) and I kept hitting it by mistake:
+      unbind !
+    '';
+  };
+
   # other programs
   programs.iotop.enable = true;
   programs.less.enable = true;
-  programs.tmux.enable = true;
   programs.traceroute.enable = true;
   environment.systemPackages = with pkgs; [
     # Utilities
