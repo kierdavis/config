c184b79dd15e6f4d95e9ec2ebcb324cd9b40fb78
Kier Davis <me@kierdavis.com>
2021-03-21 14:58:41 +0000

    Ensure command-not-found is not enabled on tiny profiles

--- nixos/extras/tiny.nix
+++ nixos/extras/tiny.nix
@@ -2,6 +2,7 @@
   nixpkgs.overlays = [ (import ../../patches/tiny.nix) ];
   boot.plymouth.enable = lib.mkForce false;
   fonts.fontconfig.enable = lib.mkForce false;
+  programs.command-not-found.enable = lib.mkForce false;
   services.udisks2.enable = lib.mkForce false;
   services.timesyncd.enable = lib.mkForce false;
   services.atd.enable = lib.mkForce false;
