ae7f702ce65100b7200417921fe4d8742d2f3918
Kier Davis <me@kierdavis.com>
2021-03-21 17:13:05 +0000

    WSL2: fix gpg build

--- patches/tiny.nix
+++ patches/tiny.nix
@@ -1,11 +1,5 @@
 self: super: {
   gnupg = super.gnupg.override {
-    # 'true' removes gpg-agent, which breaks password-store.
-    enableMinimal = false;
-    # This option's name isn't very descriptive of what it actually does.
-    # When true, gnupg uses "third-party" pinentry rather than an internal one.
-    # This is fine since we configure "third-party" pinentry to build without GUI flavours.
-    guiSupport = true;
     libusb1 = null;
     openldap = null;
     pcsclite = null;
