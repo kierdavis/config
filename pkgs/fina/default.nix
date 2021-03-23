f4da1edefa55d63a713cd7384cef143b873427c6
Kier Davis <me@kierdavis.com>
2021-03-23 02:01:10 +0000

    Update fina

--- pkgs/fina/default.nix
+++ pkgs/fina/default.nix
@@ -9,11 +9,11 @@
 
 let
   crateName = "fina";
-  version = "0.1.0";
+  version = "0.2.0";
   src = fetchCrate {
     inherit crateName version;
     # sha256 = lib.fakeSha256;
-    sha256 = "12f2ipppfq1mlmgqban1ka4pj1rsqfv2hf5x9nvzd7j6h9s1ylx7";
+    sha256 = "1n9lk0f3av68c4cdlpmvrajn0k4wjh5fw50x8qah5mb0d2hil34m";
   };
   crate = (import ./Cargo.nix {
     inherit buildRustCrate defaultCrateOverrides lib stdenv;
