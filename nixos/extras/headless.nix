d07f49a4ae58b225e856f6c26dc46aa424659615
Kier Davis <me@kierdavis.com>
2021-06-27 19:22:56 +0100

    Tidy up

--- nixos/extras/headless.nix
+++ nixos/extras/headless.nix
@@ -22,17 +22,8 @@ in {
   services.xserver.enable = mkLowPriority false;
   sound.enable = mkLowPriority false;
 
-  # Don't start a tty on the serial consoles.
-  #systemd.services."serial-getty@ttyS0".enable = false;
-  #systemd.services."serial-getty@hvc0".enable = false;
-  #systemd.services."getty@tty1".enable = false;
-  #systemd.services."autovt@".enable = false;
-
-  # Since we can't manually respond to a panic, just reboot.
-  boot.kernelParams = [ "panic=1" "boot.panic_on_fail" ];
-
   # Being headless, we don't need a GRUB splash image.
-  boot.loader.grub.splashImage = mkLowPriority null;
+  boot.loader.grub.splashImage = null;
 
   # Disable optional features of some packages to reduce dependency on graphics libraries.
   nixpkgs.overlays = [(self: super: {
