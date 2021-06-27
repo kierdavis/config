dbb3a66655b7555875d8c864a75b67ec5bf967e4
Kier Davis <me@kierdavis.com>
2021-06-27 17:25:02 +0100

    chrome dns fixes

--- nixos/extras/desktop/google-chrome.nix
+++ nixos/extras/desktop/google-chrome.nix
@@ -1,6 +1,8 @@
-# google chrome ignore /etc/hosts, so provide hist domains using a local DNS server instead.
-
 { config, lib, pkgs, ... }: {
+  programs.chromium.enable = true;
+  environment.systemPackages = [ pkgs.google-chrome ];
+
+  # Google Chrome ignore /etc/hosts, so provide hist domains using a local DNS server instead.
   environment.etc."resolv.conf".text = "nameserver 127.0.0.1";
   services.coredns.enable = true;
   services.coredns.config = ''
@@ -24,4 +26,10 @@
     }
   '';
   networking.resolvconf.extraConfig = "resolv_conf=/etc/resolv.conf.upstream";
+
+  # Hack to make Google Chrome send DNS AAAA queries in addition to A queries even if the machine has no IPv6 internet connection.
+  networking.interfaces.lo.ipv6.routes = lib.optional (!config.machine.ipv6-internet) {
+    address = "2001:4860:4860::8888";
+    prefixLength = 128;
+  };
 }
