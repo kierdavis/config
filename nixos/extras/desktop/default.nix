84b160496f1f7bfc0fde37f5bcd81328e08a8672
Kier Davis <me@kierdavis.com>
2021-09-06 20:28:13 +0100

    refactor

--- nixos/extras/desktop/default.nix
+++ nixos/extras/desktop/default.nix
@@ -3,11 +3,17 @@
 {
   imports = [
     ./apps.nix
-    ./etc.nix
     ./google-chrome.nix
+    ./i3blocks-config.nix
     ./x11.nix
   ];
 
   # Plymouth splash screen during boot.
   boot.plymouth.enable = true;
+
+  # required for fluidsynth
+  security.pam.loginLimits = [
+    { domain = "@audio"; type = "-"; item = "rtprio"; value = "90"; }
+    { domain = "@audio"; type = "-"; item = "memlock"; value = "unlimited"; }
+  ];
 }
