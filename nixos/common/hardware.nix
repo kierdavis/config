2a569c0a6b912c720e3f40e13525635c7463eee2
Kier Davis <me@kierdavis.com>
2021-10-10 15:27:55 +0100

    static name for 3d printer ttyUSB device

--- nixos/common/hardware.nix
+++ nixos/common/hardware.nix
@@ -6,5 +6,8 @@
     ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", ENV{ID_MM_DEVICE_IGNORE}="1", ENV{ID_MM_PORT_IGNORE}="1"
     ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789a]*", ENV{MTP_NO_PROBE}="1"
     KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04*", RUN:="${pkgs.coreutils}/bin/stty -F /dev/%k raw -echo"
+
+    # 3D printer
+    ACTION=="add", SUBSYSTEM=="tty", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="7523", SYMLINK+="geeetech-prusa-i3"
   '';
 }
