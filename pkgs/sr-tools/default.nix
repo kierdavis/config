17d89087e9c5972e5695f795315cb8d504c65216
Kier Davis <me@kierdavis.com>
2021-11-18 03:23:05 +0000

    repackage sr-tools (latest versions, fix tests etc)

--- pkgs/sr-tools/default.nix
+++ pkgs/sr-tools/default.nix
@@ -1,120 +1,157 @@
-{ glibcLocales, libyaml, python3Packages }:
+{ fetchFromGitHub, imagemagick, python37 }:
 
 let
-  fetchPypi = python3Packages.fetchPypi;
-
-  future = python3Packages.buildPythonPackage rec {
-    pname = "future";
-    version = "0.17.1";
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "1f2rlqn9rh7adgir52dlbqz69gsab44x0mlm8gf1cs7xvhv54137";
+  basePython = python37;
+  myPython = basePython.override {
+    packageOverrides = self: super: {
+      Babel = self.buildPythonPackage rec {
+        pname = "Babel";
+        version = "2.6.0";
+        src = self.fetchPypi {
+          inherit pname version;
+          hash = "sha256:08rxmbx2s4irp0w0gmn498vns5xy0fagm0fg33xa772jiks51flc";
+        };
+        propagatedBuildInputs = with self; [ pytz ];
+        checkInputs = with self; [ freezegun pytest3CheckHook ];
+      };
+      docutils = self.buildPythonPackage rec {
+        # Tests require Python < 3.8 because:
+        #   ODT writer uses xml.etree to generate XML.
+        #   Tests expect element attributes to be emitted in sorted order, but actually they are getting emitted in dict insertion order.
+        #   This behaviour was changed in Python 3.8.
+        #   Search "made predictable" at https://docs.python.org/3/library/xml.etree.elementtree.html
+        pname = "docutils";
+        version = "0.14";
+        src = self.fetchPypi {
+          inherit pname version;
+          hash = "sha256:0x22fs3pdmr42kvz6c654756wja305qv6cx1zbhwlagvxgr4xrji";
+        };
+        checkPhase = ''
+          cd test3
+          ./alltests.py
+        '';
+      };
+      future = self.buildPythonPackage rec {
+        # test_pow requires Python < 3.8
+        pname = "future";
+        version = "0.18.2";
+        src = self.fetchPypi {
+          inherit pname version;
+          hash = "sha256:0zakvfj87gy6mn1nba06sdha63rn4njm7bhh0wzyrxhcny8avgmi";
+        };
+        checkInputs = with self; [ pytestCheckHook ];
+        disabledTests = [
+          # Requires network access:
+          "test_moves_urllib_request_http"
+          "test_urllib_request_http"
+        ];
+      };
+      pygments = self.buildPythonPackage rec {
+        pname = "Pygments";
+        version = "2.6.1";
+        src = self.fetchPypi {
+          inherit pname version;
+          hash = "sha256:0i4gnd4q0mgkq0dp5wymn7ca8zjd8fgp63139svs6jf2c6h48wv4";
+        };
+        checkInputs = with self; [ pytestCheckHook ];
+      };
+      # Needed, otherwise pytest gets derived using our custom versions of docutils etc.
+      # This is bad because pytestCheckHook uses a fixed, non-overridable version of pytest.
+      # So if you make any changes to python.pkgs.pytest, you'll end up with multiple conflicting versions of pytest in the environment.
+      pytest = basePython.pkgs.pytest;
+      # However, we're free to define a custom pytest hook under a different name.
+      pytest3 = self.buildPythonPackage rec {
+        pname = "pytest";
+        version = "3.10.1";
+        # tar.gz tries to do some hot-downloading of a wheel.
+        format = "wheel";
+        src = self.fetchPypi {
+          inherit pname version format;
+          python = "py2.py3";
+          hash = "sha256:1v2812ixqzs4r5akzzb0c66v2cnmlfz3hf2q9jfn1lg1rzqks69z";
+        };
+        propagatedBuildInputs = with self; [
+          atomicwrites
+          attrs
+          more-itertools
+          pluggy
+          py
+          six
+        ];
+      };
+      pytest3CheckHook = self.pytestCheckHook.override { pytest = self.pytest3; };
+      sphinx = self.buildPythonPackage rec {
+        # test_correct_year requires Babel < 2.7.0 (https://github.com/python-babel/babel/commit/ea5bc4988bf7c3be84d296eb874aa11ed86c907d)
+        # test_intl requires requires docutils < 0.15 (https://github.com/sphinx-doc/sphinx/issues/6620)
+        # test_literal_include_linenos requires pygments < 2.7.0 (https://github.com/pygments/pygments/commit/728fd19eb changes the emitted HTML)
+        pname = "Sphinx";
+        version = "1.8.5";
+        src = self.fetchPypi {
+          inherit pname version;
+          hash = "sha256:c7658aab75c920288a8cf6f09f244c6cfdae30d82d803ac1634d9f223a80ca08";
+        };
+        propagatedBuildInputs = with self; [
+          alabaster
+          Babel
+          docutils
+          imagesize
+          jinja2
+          packaging
+          pygments
+          requests
+          setuptools
+          six
+          snowballstemmer
+          sphinxcontrib-websupport
+        ];
+        checkInputs = with self; [ html5lib mock imagemagick pytestCheckHook ];
+        disabledTests = [
+          # Requires network access:
+          "test_build_linkcheck"
+          "test_latex_images"
+        ];
+      };
+      sr-tools = self.buildPythonApplication rec {
+        pname = "sr.tools";
+        version = "8aac0f6b6eaedb78195b74739e5613e62bad7c75";
+        src = fetchFromGitHub {
+          owner = "srobo";
+          repo = "tools";
+          rev = version;
+          hash = "sha256:1a1sphafr1gas9x591jx2xcapw7y7dikn61ak1zqcnfdr6b8m8b4";
+        };
+        nativeBuildInputs = with self; [
+          docutils
+          pygments
+          sphinx
+        ];
+        propagatedBuildInputs = with self; [
+          beautifulsoup4
+          numpy
+          pyparsing
+          pyyaml
+          requests
+          setuptools  # not in setup.py, but needed at runtime
+          six
+          tabulate
+          xlwt-future
+        ];
+        # Stop tests trying to touch $HOME.
+        preConfigure = ''export SR_CACHE_DIR=$NIX_BUILD_TOP/sr-cache'';
+      };
+      xlwt-future = self.buildPythonPackage rec {
+        pname = "xlwt-future";
+        version = "0.8.0";
+        src = self.fetchPypi {
+          inherit pname version;
+          hash = "sha256:0zc8hhmvchywfnn893ipvmmm9mkphsj9kggrb57imzqw5p5byb55";
+        };
+        patches = [ ./xlwt-future-python36-compat.patch ];
+        propagatedBuildInputs = with self; [ future ];
+        doCheck = false;  # no tests
+        pythonImportsCheck = ["xlwt"];
+      };
     };
-    doCheck = false;  # failing :(
   };
 
-  xlwt-future = python3Packages.buildPythonPackage rec {
-    pname = "xlwt-future";
-    version = "0.8.0";
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "0zc8hhmvchywfnn893ipvmmm9mkphsj9kggrb57imzqw5p5byb55";
-    };
-    propagatedBuildInputs = with python3Packages; [
-      future
-    ];
-    doCheck = false;  # failing :(
-  };
-
-  sympy0 = python3Packages.buildPythonPackage rec {
-    pname = "sympy";
-    version = "0.7.6.1";
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "0r7mymcz3s7h7szm5v1bzdbqdqpk8w59zgzi0xyvxali22sp5hhz";
-    };
-    doCheck = false;  # failing :(
-  };
-
-  pyyaml3 = python3Packages.buildPythonPackage rec {
-    pname = "PyYAML";
-    version = "3.13";
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "3ef3092145e9b70e3ddd2c7ad59bdd0252a94dfe3949721633e41344de00a6bf";
-    };
-    propagatedBuildInputs = [ libyaml ];
-    doCheck = false;  # failing :(
-  };
-
-  sphinx1 = python3Packages.buildPythonPackage rec {
-    pname = "sphinx";
-    version = "1.8.5";
-    src = fetchPypi {
-      pname = "Sphinx";
-      inherit version;
-      sha256 = "c7658aab75c920288a8cf6f09f244c6cfdae30d82d803ac1634d9f223a80ca08";
-    };
-    LC_ALL = "en_US.UTF-8";
-
-    buildInputs = with python3Packages; [ simplejson mock glibcLocales html5lib ];
-    propagatedBuildInputs = with python3Packages; [
-      alabaster
-      Babel
-      docutils
-      imagesize
-      jinja2
-      packaging
-      pygments
-      requests
-      setuptools
-      six
-      snowballstemmer
-      sphinxcontrib-websupport
-      sqlalchemy
-      whoosh
-    ];
-
-    # Disable two tests that require network access.
-    #checkPhase = "pytest -k 'not test_defaults and not test_anchors_ignored'";
-    #checkInputs = with python3Packages; [ pytest ];
-    doCheck = false;
-
-    # https://github.com/NixOS/nixpkgs/issues/22501
-    # Do not run `python sphinx-build arguments` but `sphinx-build arguments`.
-    postPatch = ''
-      substituteInPlace sphinx/make_mode.py --replace "sys.executable, " ""
-    '';
-  };
-
-  sr-tools = python3Packages.buildPythonApplication rec {
-    pname = "sr.tools";
-    version = "1.1.2";
-    src = fetchPypi {
-      inherit pname version;
-      sha256 = "03rp6d1810iy7gxis9m001jmqy8cs0cgla149y5h7l3k1s2ragxq";
-    };
-    buildInputs = with python3Packages; [
-      nose
-      numpy
-      pygments
-      sphinx1
-    ];
-    propagatedBuildInputs = with python3Packages; [
-      beautifulsoup4
-      numpy
-      pygit2
-      pyparsing
-      pyyaml3
-      requests
-      setuptools  # needs pkg_resources at runtime
-      six
-      sympy0
-      tabulate
-      xlwt-future
-    ];
-    # Stop tests trying to touch $HOME.
-    preConfigure = ''export SR_CACHE_DIR=$NIX_BUILD_TOP/sr-cache'';
-  };
-
-in sr-tools
+in myPython.pkgs.sr-tools
