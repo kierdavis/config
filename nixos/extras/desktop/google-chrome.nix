56200de27c2372548de3f72fe9bd41fbe4bda0b6
Kier Davis <me@kierdavis.com>
2022-09-19 02:04:27 +0100

    kill off google-chrome dns/ipv6 stuff

--- nixos/extras/desktop/google-chrome.nix
+++ nixos/extras/desktop/google-chrome.nix
@@ -2,44 +2,9 @@
   programs.chromium.enable = true;
   environment.systemPackages = [ pkgs.google-chrome ];
 
-  # Google Chrome ignore /etc/hosts, so provide hist domains using a local DNS server instead.
-  environment.etc."resolv.conf".text = "nameserver 127.0.0.1";
-  services.coredns.enable = true;
-  services.coredns.config = ''
-    hist {
-      bind 127.0.0.1
-      errors
-      log
-      reload
-      cancel
-      loop
-      hosts
-    }
-    . {
-      bind 127.0.0.1
-      errors
-      log
-      reload
-      cancel
-      loop
-      forward . /etc/resolv.conf.upstream
-    }
-  '';
-  networking.resolvconf = {
-    enable = true;
-    extraConfig = "resolv_conf=/etc/resolv.conf.upstream";
-  };
-  systemd.services.coredns = {
-    after = ["dhcpcd.service"];
-    wantedBy = ["network-online.target"];
-    startLimitBurst = 1;
-    startLimitIntervalSec = 4;
-    serviceConfig.RestartSec = 5;
-  };
-
   # Hack to make Google Chrome send DNS AAAA queries in addition to A queries even if the machine has no IPv6 internet connection.
-  networking.interfaces.lo.ipv6.routes = lib.optional (!config.machine.ipv6-internet) {
-    address = "2001:4860:4860::8888";
-    prefixLength = 128;
-  };
+  #networking.interfaces.lo.ipv6.routes = lib.optional (!config.machine.ipv6-internet) {
+  #  address = "2001:4860:4860::8888";
+  #  prefixLength = 128;
+  #};
 }
