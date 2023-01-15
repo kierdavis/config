19e3be87f2d289508f1623a6c71ff488a976c779
Kier Davis <me@kierdavis.com>
2023-01-15 17:10:26 +0000

    fix ceph mounts after migration

--- nixos/common/fs.nix
+++ nixos/common/fs.nix
@@ -29,23 +29,23 @@
   # By default this is in /etc, which is not very NixOS-friendly.
   environment.variables.LVM_SYSTEM_DIR = "/var/lvm";
 
-  #fileSystems."/net/skaia/media" = {
-  #  fsType = "ceph";
-  #  device = "10.88.234.14,10.88.200.103,10.88.225.6:/volumes/csi/csi-vol-a1eb5fea-8788-11ed-96c1-aaf4d0a0d0ed/4321c932-e5cd-4b2b-ad0f-56d57379f7c6";
-  #  options = [
-  #    "name=${config.networking.hostName}"
-  #    "secretfile=/etc/ceph-client-secret"
-  #  ];
-  #};
-  #fileSystems."/net/skaia/torrent-downloads" = {
-  #  fsType = "ceph";
-  #  device = "10.88.234.14,10.88.200.103,10.88.225.6:/volumes/csi/csi-vol-d2f2e43d-6926-11ed-b262-a6adf78fc049/7ab73f0e-d778-4c28-ac8b-041f63381ff1";
-  #  options = [
-  #    "name=${config.networking.hostName}"
-  #    "secretfile=/etc/ceph-client-secret"
-  #    "ro"
-  #  ];
-  #};
+  fileSystems."/net/skaia/media" = {
+    fsType = "ceph";
+    device = "10.88.227.116,10.88.231.188,10.88.214.239:/volumes/csi/csi-vol-a1eb5fea-8788-11ed-96c1-aaf4d0a0d0ed/4321c932-e5cd-4b2b-ad0f-56d57379f7c6";
+    options = [
+      "name=${config.networking.hostName}"
+      "secretfile=/etc/ceph-client-secret"
+    ];
+  };
+  fileSystems."/net/skaia/torrent-downloads" = {
+    fsType = "ceph";
+    device = "10.88.227.116,10.88.231.188,10.88.214.239:/volumes/csi/csi-vol-cafbb089-9385-11ed-8c0c-aabb135b14ca/bc3d7cd7-1f6d-447b-b659-53dc9f0f401a";
+    options = [
+      "name=${config.networking.hostName}"
+      "secretfile=/etc/ceph-client-secret"
+      "ro"
+    ];
+  };
 
   # UID/GID used for files on CephFS filesystems where permissioning doesn't matter.
   users.groups.cephfsdata = {
