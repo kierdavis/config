c4e8fb4d1755cb27edfb1692c7860b3c3e2f8f6a
Kier Davis <me@kierdavis.com>
2024-11-11 15:13:46 +0000

    add gcc

--- nixos/extras/devel.nix
+++ nixos/extras/devel.nix
@@ -11,6 +11,7 @@ in {
   environment.systemPackages = with pkgs; [
     # Software
     cargo
+    gcc  # provides linker for cargo
     (python3.withPackages (pyPkgs: with pyPkgs; [ virtualenv ]))
 
     # Infrastructure
