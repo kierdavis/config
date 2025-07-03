daae2248dcc025a9bae298a658189a929434eec8
Kier Davis <me@kierdavis.com>
2025-07-03 04:57:52 +0100

    flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,3 +1,25 @@
 {
   description = "config/nixos";
+
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "kierdavis";
+      repo = "nixpkgs";
+      rev = "b43c397f6c213918d6cfe6e3550abfe79b5d1c51";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      coloris = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ machines/coloris.nix ];
+      };
+      saelli = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ machines/saelli.nix ];
+      };
+    };
+  };
 }
